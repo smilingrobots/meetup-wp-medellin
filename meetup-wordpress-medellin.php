94d05facbe128df2187f46b44c444155e887e6c7
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-10-15 21:45:53 -0500

    Agrega función para general el código HTML del formulario  de reporte

--- meetup-wordpress-medellin.php
+++ meetup-wordpress-medellin.php
@@ -7,7 +7,6 @@
  */
 
 function mwpm_generate_button_for_post( $post_id ) {
-    $user_id = get_current_user_id();
     $button  = '';
     $button .= '<button id="reportBtn">';
     $button .= '<span class="dashicons dashicons-flag"></span>';
@@ -69,6 +68,7 @@ function mwpm_maybe_send_report() {
 add_action( 'wp', 'mwpm_maybe_send_report' );
 
 function mwpm_add_modal_to_footer() {
+    $user_id = get_current_user_id();
     $modal = '
     <!-- Report Post Modal -->
     <div id="reportModal" class="report-modal">
@@ -77,27 +77,8 @@ function mwpm_add_modal_to_footer() {
                 <h2>You are about to report "' . get_the_title() . '"</h2>
             </span>
             <span class="close">&times;</span>
-
-            <form action="/" method="post">
-                <input type="hidden" name="report[post_id]" value="' . get_the_ID() . '" />
-                <p>
-                    <label for="mwpm-report-form__name">' . __( 'Your Name', 'mwpm' ) . '</label>
-                    <input id="mwpm-report-form__name" type="text" name="report[name]" placeholder="María Pérez" required/>
-                </p>
-                <p>
-                    <label for="mwpm-report-form__email">' . __( 'Your Email', 'mwpm' ) . '</label>
-                    <input id="mwpm-report-form__email" type="email" name="report[email]" placeholder="maria.perez@example.org" required/>
-                </p>
-
-                <p>
-                    <label for="mwpm-report-form__reason">' . __( 'Please enter the reasons to report this post:', 'mwpm' ) . '</label>
-                    <textarea id="mwpm-report-form__reason" name="report[reason]" value="" placeholder="' . __( 'Additional info.', 'mwpm' ) . '" required></textarea>
-                </p>
-                <p>
-                    <input class="btn" type="submit" value="' . __( 'Report', 'mwpm' ) . '" />
-                </p>
-            </form>
-        </div>
+            ' . get_modal_form() .
+        '</div>
     </div>';
 
     echo $modal;
@@ -129,4 +110,57 @@ function mwpm_get_headers( $report ) {
     $headers[] = 'Content-Type: text/html';
 
     return $headers;
+}
+
+function get_modal_form() {
+    // Inicializar variables y obtener los datos del usuario actual
+    $username     = '';
+    $user_email   = '';
+    $current_user = wp_get_current_user();
+
+    // Validar que el usuario esté loggeado.
+    if ( $current_user->exists() ) {
+        // Obtener los datos del usuario actual.
+        $username = $current_user->user_login;
+        $user_email = $current_user->user_email;
+    }
+
+    // Crear campo del nombre de quien reporta
+    $name_input = '<label for="mwpm-report-form__name">' . __( 'Your Name', 'mwpm' ) . ': </label>'. $username ;
+
+    if ( empty( $username ) ) {
+        $name_input .= '<input id="mwpm-report-form__name" type="text" name="report[name]" placeholder="María Pérez" required/>';
+    } else {
+        $name_input .= '<input id="mwpm-report-form__name" type="hidden" name="report[name]" value=" . ' . $username . '"/>';
+    }
+
+    // Crear campo del correo de quien reporta
+    $email_input = '<label for="mwpm-report-form__email">' . __( 'Your Email', 'mwpm' ) . ': </label>' . $user_email;
+
+    if ( empty( $user_email ) ) {
+        $email_input .= '<input id="mwpm-report-form__email" type="email" name="report[email]" placeholder="maria.perez@example.org" required/>';
+    } else {
+        $email_input .= '<input id="mwpm-report-form__email" type="hidden" name="report[email]" value="' . $user_email . '"/>';
+    }
+
+    // Crear formulario.
+    $form = '<form action="/" method="post">
+        <input type="hidden" name="report[post_id]" value="' . get_the_ID() . '" />
+        <p>
+            ' . $name_input . '
+        </p>
+        <p>
+            ' . $email_input . '
+        </p>
+
+        <p>
+            <label for="mwpm-report-form__reason">' . __( 'Please enter the reasons to report this post:', 'mwpm' ) . '</label>
+            <textarea id="mwpm-report-form__reason" name="report[reason]" value="" placeholder="' . __( 'Additional info.', 'mwpm' ) . '" required></textarea>
+        </p>
+        <p>
+            <input class="btn" type="submit" value="' . __( 'Report', 'mwpm' ) . '" />
+        </p>
+    </form>';
+
+    return $form;
 }
