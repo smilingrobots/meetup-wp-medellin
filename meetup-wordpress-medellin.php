d9236a97ac965b2dad12b8f867aef29b384c9a88
Willington Vega <wvega@wvega.com>
2018-10-28 16:07:24 -0500

    Agregar un filtro para modificar el contenido del post.

--- meetup-wordpress-medellin.php
+++ meetup-wordpress-medellin.php
@@ -5,3 +5,19 @@
  * Description: Plugin de prueba para el taller de introducción al desarrollo de plugins para WordPress.
  * Version: 1.0.0
  */
+
+function mwpm_add_report_button_to_content( $content ) {
+    // El formulario para reportar entradas solo será mostrado en páginas y
+    // publicaciones individuales.
+    if ( is_single() || is_page() ) {
+        $report_button  = '';
+        $report_button .= '<p>';
+        $report_button .= '-- Reportar --';
+        $report_button .= '</p>';
+
+        return $report_button . $content;
+    }
+
+    return $content;
+}
+add_filter( 'the_content', 'mwpm_add_report_button_to_content' );
