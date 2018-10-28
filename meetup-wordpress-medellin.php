65d77232f0eb5d3fe0bb20636d7540bf661e9c80
Willington Vega <wvega@wvega.com>
2018-10-28 16:01:32 -0500

    Crear el archivo del plugin.

---
+++ meetup-wordpress-medellin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Meetup Wordpress Medellín 2018
+ * Plugin URI: https://github.com/smilingrobots/meetup-wp-medellin
+ * Description: Plugin de prueba para el taller de introducción al desarrollo de plugins para WordPress.
+ * Version: 1.0.0
+ */
