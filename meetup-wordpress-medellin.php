f867782eddc3a057007b70fb7b0789c1f0804ba8
Willington Vega <wvega@wvega.com>
2018-10-28 17:20:19 -0500

    Agregar formulario para reportar entradas.

--- meetup-wordpress-medellin.php
+++ meetup-wordpress-medellin.php
@@ -15,7 +15,9 @@ function mwpm_add_report_button_to_content( $content ) {
         $report_button .= mwpm_generate_button_for_post();
         $report_button .= '</p>';
 
-        return $report_button . $content;
+        $report_modal = mwpm_render_report_modal();
+
+        return $report_button . $content . $report_modal;
     }
 
     return $content;
@@ -31,4 +33,78 @@ function mwpm_generate_button_for_post() {
     $button .= '</button>';
 
     return $button;
+}
+
+/**
+ * Crea el código HTML para el cuadro de diálogo que los usuarios podrán utilizar
+ * para reportar una entrada.
+ *
+ * @since 1.0.0
+ */
+function mwpm_render_report_modal() {
+    $modal = '
+    <!-- Report Post Modal -->
+    <div id="reportModal" class="report-modal">
+        <div class="report-modal-content">
+            <span class="report-header">
+                <h2>You are about to report "' . get_the_title() . '"</h2>
+            </span>
+            <span class="close">&times;</span>
+            ' . mwpm_get_modal_form() . '
+        </div>
+    </div>';
+
+
+    return $modal;
+}
+
+/**
+ * Construye el código HTML para el formulario que los usuarios podrán utilizar para
+ * describir las razones por las que están reportando una entrada.
+ *
+ * @since 1.0.0
+ */
+function mwpm_get_modal_form() {
+    $form = '
+    <form action="" method="post">
+        <input type="hidden" name="report[post_id]" value="' . get_the_ID() . '" />
+        <p>' . mwpm_render_name_input_field() . '</p>
+        <p>' . mwpm_render_email_input_field() . '</p>
+
+        <p>
+            <label for="mwpm-report-form__reason">Please enter the reasons to report this post:</label>
+            <textarea id="mwpm-report-form__reason" name="report[reason]" value="" placeholder="Additional info." required></textarea>
+        </p>
+        <p>
+            <input class="btn" type="submit" value="Report" />
+        </p>
+    </form>';
+
+    return $form;
+}
+
+/**
+ * Crea el código HTML para el campo del nombre de quien reporta.
+ *
+ * @since 1.0.0
+ */
+function mwpm_render_name_input_field() {
+    $field  = '';
+    $field .= '<label for="mwpm-report-form__name">Your Name</label>';
+    $field .= '<input id="mwpm-report-form__name" type="text" name="report[name]" placeholder="María Pérez" required/>';
+
+    return $field;
+}
+
+/**
+ * Crea el código HTML para el campo del correo electrónico de quien reporta.
+ *
+ * @since 1.0.0
+ */
+function mwpm_render_email_input_field() {
+    $field  = '';
+    $field .= '<label for="mwpm-report-form__email">You Email</label>';
+    $field .= '<input id="mwpm-report-form__email" type="email" name="report[email]" placeholder="maria.perez@example.org" required/>';
+
+    return $field;
 }
